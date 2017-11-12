d1db1aaa4fe93f3ce7ea1d733ad6a7ddde79fc53
Jevin Eyman <jevineyman@Jevins-MacBook-Pro.local>
2017-11-11 23:02:39 -0500

    unfinished work

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+//Arrays and Variables 
+//==============================================================
+var computerChoices = ["a", "b", "c", "d", "e","f","g","h", "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// Creating variables to hold the number of wins and losses. They start at 0. 
+    var wins = 0;
+    var losses = 0;
+    
+     document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+
+	var computerGuess = computerChoices[Math.floor(Math.random()*26)+1; computerChoices.length)];
+
+}
+
+
+
+
+
+
+//Functions
+//===============================================================
+
+
+
+
+
+//Main Process
+//===============================================================
+
+
+
+
+      });
